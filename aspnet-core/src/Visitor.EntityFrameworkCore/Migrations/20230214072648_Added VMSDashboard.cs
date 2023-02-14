81bdb093253651390062324ab14bde3be2d787fb
Hafiy Kharu <muhamadhafiykharu@gmail.com>
2023-02-14 15:40:08 +0800

    Add table vms dashboard to database

---
+++ aspnet-core/src/Visitor.EntityFrameworkCore/Migrations/20230214072648_Added VMSDashboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Visitor.Migrations
+{
+    public partial class AddedVMSDashboard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VMSDashboards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenantId = table.Column<int>(type: "int", nullable: true),
+                    AppoitnmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    AppointmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VMSDashboards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VMSDashboards_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VMSDashboards_AppointmentId",
+                table: "VMSDashboards",
+                column: "AppointmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropTable(
+                name: "VMSDashboards");
+
+            
+        }
+    }
+}
